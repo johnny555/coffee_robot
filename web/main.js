0a940e872860367977eb9da45f22314defd4a115
John Vial <badwolf.johnnyv@gmail.com>
2020-07-19 04:13:18 -0700

    Adding rplidar

--- web/main.js
+++ web/main.js
@@ -1,77 +1,67 @@
-let vueApp = new Vue({
-    el: "#vueApp",
+var app = new Vue({
+    el: '#app',
+    // storing the state of the page
     data: {
-        // ros connection
-        ros: null,
-        rosbridge_address: 'wss://i-0734dfc7411934198.robotigniteacademy.com/rosbridge/',
         connected: false,
-        // page content
-        menu_title: 'Connection',
-        main_title: 'Main title, from Vue!!',
+        ros: null,
+        logs: [],
+        loading: false,
+        rosbridge_address: 'wss://i-0d9a83fef6f780c4d.robotigniteacademy.com/rosbridge/',
+        port: '9090',
+        mapViewer: null,
+        mapGridClient: null,
+        interval: null,
     },
+    // helper methods to connect to ROS
     methods: {
         connect: function () {
-            // define ROSBridge connection object
+            this.loading = true
             this.ros = new ROSLIB.Ros({
                 url: this.rosbridge_address
             })
-
-            // define callbacks
             this.ros.on('connection', () => {
+                this.logs.unshift((new Date()).toTimeString() + ' - Connected!')
                 this.connected = true
-                console.log('Connection to ROSBridge established!')
+                this.loading = false
+
+                this.mapViewer = new ROS2D.Viewer({
+                    divID: 'map',
+                    width: 420,
+                    height: 360
+                })
+
+                // Setup the map client.
+                this.mapGridClient = new ROS2D.OccupancyGridClient({
+                    ros: this.ros,
+                    rootObject: this.mapViewer.scene,
+                    continuous: true,
+                })
+                // Scale the canvas to fit to the map
+                this.mapGridClient.on('change', () => {
+                    this.mapViewer.scaleToDimensions(this.mapGridClient.currentGrid.width, this.mapGridClient.currentGrid.height);
+                    this.mapViewer.shift(this.mapGridClient.currentGrid.pose.position.x, this.mapGridClient.currentGrid.pose.position.y)
+                })
+
             })
             this.ros.on('error', (error) => {
-                console.log('Something went wrong when trying to connect')
-                console.log(error)
+                this.logs.unshift((new Date()).toTimeString() + ` - Error: ${error}`)
             })
             this.ros.on('close', () => {
+                this.logs.unshift((new Date()).toTimeString() + ' - Disconnected!')
                 this.connected = false
-                console.log('Connection to ROSBridge was closed!')
+                this.loading = false
+                document.getElementById('map').innerHTML = ''
             })
         },
         disconnect: function () {
             this.ros.close()
         },
-        sendCommand: function () {
-            let topic = new ROSLIB.Topic({
-                ros: this.ros,
-                name: '/cmd_vel',
-                messageType: 'geometry_msgs/Twist'
-            })
-            let message = new ROSLIB.Message({
-                linear: { x: 1, y: 0, z: 0, },
-                angular: { x: 0, y: 0, z: 0.5, },
-            })
-            topic.publish(message)
-        },
-        turnRight: function () {
-            let topic = new ROSLIB.Topic({
-                ros: this.ros,
-                name: '/cmd_vel',
-                messageType: 'geometry_msgs/Twist'
-            })
-            let message = new ROSLIB.Message({
-                linear: { x: 1, y: 0, z: 0, },
-                angular: { x: 0, y: 0, z: -0.5, },
-            })
-            topic.publish(message)
-        },
-        stop: function () {
-            let topic = new ROSLIB.Topic({
-                ros: this.ros,
-                name: '/cmd_vel',
-                messageType: 'geometry_msgs/Twist'
-            })
-            let message = new ROSLIB.Message({
-                linear: { x: 0, y: 0, z: 0, },
-                angular: { x: 0, y: 0, z: 0, },
-            })
-            topic.publish(message)
-        },
     },
     mounted() {
-        // page is ready
-        console.log('page is ready!')
+        this.interval = setInterval(() => {
+            if (this.ros != null && this.ros.isConnected) {
+                this.ros.getNodes((data) => { }, (error) => { })
+            }
+        }, 10000)
     },
 })
